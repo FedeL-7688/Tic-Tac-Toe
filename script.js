a43c528a1c1933b3f220b1285b7e06fe01512801
FedeL <115208276+FedeL-7688@users.noreply.github.com>
2026-02-10 18:03:37 -0300

    still working on the idea of console game

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 ////Tic-Tac-Toe game
 //// 1) create table
+//// 1.2) create a way to replace a number with an X or O
+//// 1.2.1) 
 //// 2) input the player and CPU objects so their choices load up in the board (first in console)
 //// 3) create game systems to count wins from each players and set a max of 5 rounds
+
 
 const GameBoard =(function () {
   const maker = {
     board: [1, 2, 3, 4, 5, 6, 7, 8, 9]
   };
   function markSetter(index,mark){
-    board[index]= mark
-    return board
+    maker.board[index]= mark;
+    
   }
 
   function gridMaker() {
@@ -24,26 +27,64 @@ const GameBoard =(function () {
     return output
   }
 
+  function gridDel(place){
+   let index = maker.board.indexOf(place)
+    maker.board.splice(index,1)
+  }
   gridMaker(maker.board);
 
 
   return {
+    gridDel,
     markSetter,
     gridMaker
   }
+
+
 })();
 
+const player = function(name){
+ const Pname = name
+ let turn = 0
+  getTurn = function(){
+    console.log(turn)
+  }
+  addTurn = function() {turn++}
+ return{
+  Pname,getTurn,addTurn
+ }
+}
+
+function game(p1, p2) {
+  console.log("Welcome to tic tac toe")
+  let player1 = player(p1)
+  let player2 = player(p2)
+function play(player)
+{console.log(`player ${player.Pname}, make your move`)
+console.log("available options: " )
+}
+const score = function(){
+ 
+}
+player1.addTurn()
+player1.addTurn()
+player1.getTurn()
 
 
-function game(p1, p2) {}
-
-
-board = GameBoard.gridMaker()
-console.log(board)
-
-
-GameBoard.markSetter(1,'hi')
-console.log(board)
+}
+game("pepe","juan")
 
 
 
+
+GameBoard.gridMaker()
+GameBoard.gridDel(2)
+GameBoard.gridMaker()
+
+
+
+
+
+
+
+// console.log("\x1b[37mTurno del jugador X\x1b[0m");
